b79982404cf47ef655061b1d8d42afbba6e6249e
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-01-10 00:17:05 +0200

    Create upgradenode.sh

---
+++ upgradenode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo systemctl stop celestia-bridge
+rm -rf celestia-node
+git clone https://github.com/celestiaorg/celestia-node.git
+cd celestia-node/
+git checkout tags/v0.20.4-mocha
+make build 
+sudo make install 
+make cel-key 
+celestia bridge config-update --p2p.network mocha
+systemctl daemon-reload
+systemctl restart celestia-bridge && journalctl -u celestia-bridge -f -o cat
