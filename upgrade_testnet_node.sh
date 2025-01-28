817e09fae248c9db6fdbbd007995f7be5b52393d
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-01-28 21:13:04 +0200

    Update upgrade_testnet_node.sh

--- upgrade_testnet_node.sh
+++ upgrade_testnet_node.sh
@@ -3,7 +3,7 @@ sudo systemctl stop celestia-bridge
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
-git checkout tags/v0.20.4-mocha
+git checkout tags/v0.21.3-mocha
 make build 
 sudo make install 
 make cel-key 
