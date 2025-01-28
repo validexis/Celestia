6433412d7213bedc7398def23225e9849c034705
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-01-28 21:12:44 +0200

    Update upgrade_testnet_app.sh

--- upgrade_testnet_app.sh
+++ upgrade_testnet_app.sh
@@ -3,7 +3,7 @@ cd $HOME
 rm -rf celestia-app 
 git clone https://github.com/celestiaorg/celestia-app.git 
 cd celestia-app/ 
-APP_VERSION=v3.2.0-mocha
+APP_VERSION=v3.3.0-mocha
 git checkout tags/$APP_VERSION -b $APP_VERSION 
 make install
 sudo systemctl restart celestia-appd && sudo journalctl -u celestia-appd -f
