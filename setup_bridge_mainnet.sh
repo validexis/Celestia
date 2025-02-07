b66005736f8789da12900a732ceaf98a1e9af799
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-02-07 20:43:52 +0200

    Update setup_bridge_mainnet.sh

--- setup_bridge_mainnet.sh
+++ setup_bridge_mainnet.sh
@@ -13,7 +13,7 @@ cd $HOME
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
-git checkout tags/v0.20.4 
+git checkout tags/v0.21.5 
 make build 
 sudo make install 
 make cel-key
