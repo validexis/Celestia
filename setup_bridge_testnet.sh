02cfdabbaf4842a125c3999e24cd2125ff57ec7e
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-02-11 12:27:55 +0200

    Update setup_bridge_testnet.sh

--- setup_bridge_testnet.sh
+++ setup_bridge_testnet.sh
@@ -13,7 +13,7 @@ cd $HOME
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node
 cd celestia-node/
-git checkout tags/v0.21.3-mocha
+git checkout tags/v0.21.5-mocha
 make build 
 sudo make install 
 make cel-key
