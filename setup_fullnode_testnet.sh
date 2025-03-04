0a693688d713501ce8eced2e6988ada0b6719ab7
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-03-04 17:35:35 +0200

    Update setup_fullnode_testnet.sh

--- setup_fullnode_testnet.sh
+++ setup_fullnode_testnet.sh
@@ -13,7 +13,7 @@ cd $HOME
 rm -rf celestia-app
 git clone https://github.com/celestiaorg/celestia-app.git
 cd celestia-app
-git checkout v3.3.1-mocha
+git checkout v3.4.0-mocha
 make install
 
 celestia-appd config chain-id mocha-4
