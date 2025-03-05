f0c064cba883994aaae320baaf64b8b046e7772a
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-03-05 17:04:10 +0200

    Update upgrade_mainnet_app.sh

--- upgrade_mainnet_app.sh
+++ upgrade_mainnet_app.sh
@@ -3,7 +3,7 @@ cd $HOME
 rm -rf celestia-app 
 git clone https://github.com/celestiaorg/celestia-app.git 
 cd celestia-app/ 
-APP_VERSION=v3.3.1
+APP_VERSION=v3.4.0
 git checkout tags/$APP_VERSION -b $APP_VERSION 
 make install
 sudo systemctl restart celestia-appd && sudo journalctl -u celestia-appd -f
